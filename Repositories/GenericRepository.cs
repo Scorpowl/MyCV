fbe1a5ae3ee3786c1e964c5aff212688f5db44ce
Furkan TOSUN <furkann.1777@outlook.com>
2025-04-29 01:03:21 +0300

    GenericRepository

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyCV.Models.Entity;
+
+namespace MyCV.Repositories
+{
+    public class GenericRepository<T> where T: class, new()
+    {
+        MvcCvEntities db = new MvcCvEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+        public void TUpdate(T p)
+        {
+            db.SaveChanges();
+        }
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+
+    }
+}
