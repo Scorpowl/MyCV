8970af3ac5a1a26208488b77bce3c85fb907b79e
Furkan TOSUN <furkann.1777@outlook.com>
2025-04-29 01:16:47 +0300

    deneyim reposuna başlangıç

--- Repositories/DeneyimRepository.cs
+++ Repositories/DeneyimRepository.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MyCV.Models.Entity;
 
 namespace MyCV.Repositories
 {
     public class DeneyimRepository
     {
+        MvcCvEntities db = new MvcCvEntities();
+        public List<TblDeneyimlerim> List()
+        {
+            return db.TblDeneyimlerim.ToList();
+        }
     }
 }
