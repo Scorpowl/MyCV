b98bce7b16cc1d9090e29624d5c18f0a9ccaefa9
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-05 01:03:00 +0300

    derecelerin listelenmesi dinamik hale getirildi.

--- Controllers/YetenekController.cs
+++ Controllers/YetenekController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyCV.Repositories;
+using MyCV.Models.Entity;
 
 namespace MyCV.Controllers
 {
     public class YetenekController : Controller
     {
         // GET: Yetenek
+        
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim> ();
+
         public ActionResult Index()
         {
-            return View();
+            var yetenekler = repo.List();
+            return View(yetenekler);
         }
     }
 }
