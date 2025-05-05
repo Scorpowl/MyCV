79d29550e229a0ba17b61d5e36584aa78c3606f7
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-06 00:04:04 +0300

    eğitim kısmı tamamlandı

---
+++ Controllers/EgitimController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCV.Models.Entity;
+using MyCV.Repositories;
+
+namespace MyCV.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            repo.TAdd(t);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDüzenle(int id)
+        {
+            TblEgitimlerim t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult EgitimDüzenle(TblEgitimlerim p)
+        {
+            TblEgitimlerim t = repo.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
+            t.AltBaslik1 = p.AltBaslik1;
+            t.AltBaslik2 = p.AltBaslik2;
+            t.Tarih = p.Tarih;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+
+
+}
