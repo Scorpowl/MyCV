87fffe5212fa7e5f0c63870e984175251f6878d0
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-05 03:01:25 +0300

    Düzenleme işlemi yapıldı

--- Controllers/YetenekController.cs
+++ Controllers/YetenekController.cs
@@ -11,13 +11,45 @@ namespace MyCV.Controllers
     public class YetenekController : Controller
     {
         // GET: Yetenek
-        
-        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim> ();
+
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
 
         public ActionResult Index()
         {
             var yetenekler = repo.List();
             return View(yetenekler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id); // önce bul
+            repo.TDelete(yetenek);                    // sonra sil
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekDüzenle(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekDüzenle(TblYeteneklerim t)
+        {
+            var yetenek = repo.Find(x => x.ID == t.ID);
+            yetenek.Yetenek = t.Yetenek;
+            yetenek.Oran = t.Oran;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
