3eaf0b93fef2f01eafcc52614c01eee6a1849bff
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-07 23:35:30 +0300

    sertifika güncelleme sayfası eklendi

--- Controllers/SertifikaController.cs
+++ Controllers/SertifikaController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyCV.Models.Entity;
+using MyCV.Repositories;
 
 namespace MyCV.Controllers
 {
     public class SertifikaController : Controller
     {
         // GET: Sertifika
+        GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();   
         public ActionResult Index()
         {
-            return View();
+            var sertifika = repo.List();
+            return View(sertifika);
         }
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika = repo.Find(x => x.ID == id);
+            return View(sertifika);
+        }
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarim p)
+        {
+            var sertifika = repo.Find(x => x.ID == p.ID);
+            sertifika.Aciklama = p.Aciklama;
+            sertifika.Tarih = p.Tarih;
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+        }
+
+
+
+
     }
 }
