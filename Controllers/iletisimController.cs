0f44a236d74789848d6a898e5c8d023a7b24775f
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-08 23:55:11 +0300

    iletişim controller düzenmlenmesi

--- Controllers/iletisimController.cs
+++ Controllers/iletisimController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyCV.Models.Entity;
+using MyCV.Repositories;
 
 namespace MyCV.Controllers
 {
     public class iletisimController : Controller
     {
         // GET: iletisim
+        GenericRepository<TblIletisim> repo = new GenericRepository<TblIletisim>();
         public ActionResult Index()
         {
             return View();
