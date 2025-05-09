f15a696031620f10f1e4cb7ae72d9a62e8484604
Furkan TOSUN <furkann.1777@outlook.com>
2025-05-09 14:35:46 +0300

    Hakkımda kısmı tamamlandı

--- Controllers/HakkimdaController.cs
+++ Controllers/HakkimdaController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyCV.Models.Entity;
+using MyCV.Repositories;
 
 namespace MyCV.Controllers
 {
     public class HakkimdaController : Controller
     {
         // GET: Hakkında
+        GenericRepository<TblHakkimda> repo = new GenericRepository<TblHakkimda>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHakkimda t)
+        {
+            var hakkimda = repo.Find(x => x.ID == 1);
+            hakkimda.Ad = t.Ad;
+            hakkimda.Soyad = t.Soyad;
+            hakkimda.Adres = t.Adres;
+            hakkimda.Mail = t.Mail;
+            hakkimda.Telefon = t.Telefon;
+            hakkimda.Aciklama = t.Aciklama;
+            hakkimda.Foto = t.Foto;
+            repo.TUpdate(hakkimda);
+            return RedirectToAction("Index");
         }
     }
 }
